aa63f4c7893de317c50f2598836946520ab70de6
GergesSayed <gergessayed@gmail.com>
2026-09-20 12:20:35 +0300

    feat(data): 7th pass forensic audit - purge 81 non-fleet entities (v248.0)

--- crm/js/storage.js
+++ crm/js/storage.js
@@ -780,7 +780,7 @@ const AppStorage = {
             return;
         }
         try {
-            this._worker = new Worker('js/companies-worker.js?v=247.0');
+            this._worker = new Worker('js/companies-worker.js?v=248.0');
             this._worker.onmessage = (e) => {
                 const { action, queryId, items, total, totalPages, page, pageSize } = e.data || {};
                 if (action === 'INDEX_READY' || action === 'UPDATE_DONE') {
